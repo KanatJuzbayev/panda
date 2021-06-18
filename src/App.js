b107090a452242564eefcf5d8f87e76d00db3735
Kanat Juzbayev <76990478+KanatJuzbayev@users.noreply.github.com>
2021-06-18 21:31:54 +0600

    add dear

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Happy hanuca, beach!
+          Happy hanuca, my dear!
         </p>
         <a
           className="App-link"
